d10a653e50528404dca1c895d28f4402ca31a5de
Anna <s1094208@student.hsleiden.nl>
2017-06-06 09:51:26 +0200

    Aantekeningen toegevoegd in ApiApplication

--- src/main/java/api/ApiApplication.java
+++ src/main/java/api/ApiApplication.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class ApiApplication {
 
     public static void main(String[] args) {
+        //Maak alle andere klassen aan
         VacatureDAO vacatureDAO = new VacatureDAO();
         VacatureService vacatureService = new VacatureService(vacatureDAO);
         VacatureResource vacatureResource = new VacatureResource(vacatureService);
@@ -24,27 +25,33 @@ public class ApiApplication {
         ProfielService profielService = new ProfielService(profielDAO);
         ProfielResource profielResource = new ProfielResource(profielService);
 
+        //Haal de lijst met alle vacatures op en print de titels
         List<Vacature> vacatures = (List)vacatureResource.retrieveAll();
         for (int i = 0; i < vacatures.size(); i++) {
             System.out.println(vacatures.get(i).getTitel());
         }
 
         System.out.println();
+        //Print de titel van de vacature met id 3
         System.out.println(vacatureResource.get(3).getTitel());
 
         System.out.println();
 
+        //Haal de lijst met alle profielen op en print de gebruikersnamen
         List<Profiel> profielen = (List)profielResource.retrieveAll();
         for (int i = 0; i < profielen.size(); i++) {
             System.out.println(profielen.get(i).getGebruikersnaam());
         }
 
         System.out.println();
+        //Haal het profiel met id 1 op en print de gebruikersnaam en het wachtwoord
         Profiel profiel = profielResource.get(1);
         System.out.println(profiel.getGebruikersnaam());
         System.out.println(profiel.getWachtwoord());
+        //Verander het wachtwoord van het profiel
         profiel.setWachtwoord("hetnieuwewachtwoord01");
 
+        //Wijzig het wachtwoord in de lijst en print het wachtwoord
         profielResource.wijzigWachtwoord(profiel);
         System.out.println();
         System.out.println(profielResource.get(1).getWachtwoord());
