e78f6338dc2fa67bfb6ba265031e876b5902b605
ronantalboom <Ronantalboom@hotmail.com>
2017-06-13 14:40:10 +0200

    branche en expertise tussentabellen.

--- src/main/java/fastcatch/api/db/BrancheDAO.java
+++ src/main/java/fastcatch/api/db/BrancheDAO.java
@@ -4,7 +4,9 @@ import fastcatch.api.core.Branche;
 import fastcatch.api.core.Expertise;
 import fastcatch.api.core.mappers.BrancheMapper;
 import fastcatch.api.core.mappers.ExpertiseMapper;
+import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 
 import java.util.List;
@@ -17,5 +19,12 @@ public interface BrancheDAO {
 
     @SqlQuery("select * from branche")
     List<Branche> getBranches();
+
+    @SqlUpdate("INSERT INTO gebruiker_branche (BranchebrancheType, gebruikerID)" +
+            "VALUES (:branche, :gebruikerID)")
+    void insertBrancheGebruiker(@Bind("branche") String branche, @Bind("gebruikerID") int gebruikerID);
+
+
+
 
 }
